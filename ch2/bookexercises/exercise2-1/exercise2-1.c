ace041aa9cf378a707c8d8bd08bcafcf92d971f1
John Pierce Parker <jPiroshky@gmail.com>
2012-02-29 00:19:24 -0800

    updated exercise2-1 "limits.h"
    Now the program correctly computes the limits of the basic integer types as well as print them from limits.h. Still to do: add floating point limits.

--- ch2/bookexercises/exercise2-1/exercise2-1.c
+++ ch2/bookexercises/exercise2-1/exercise2-1.c
@@ -8,12 +8,13 @@ Harder if you compute them: determine the ranges of the various
 floating-point types. */
 int main(void)
 {
+	//print values from limits.h
 	printf("from limits.h:\n");
 	printf("CHAR_BIT\t%d\n", CHAR_BIT);
 	printf("CHAR_MAX\t%d\n", CHAR_MAX);
 	printf("CHAR_MIN\t%d\n", CHAR_MIN);
-	printf("INT_MAX\t%d\n", INT_MAX);
-	printf("INT_MIN\t%d\n", INT_MIN);
+	printf("INT_MAX \t%d\n", INT_MAX);
+	printf("INT_MIN \t%d\n", INT_MIN);
 	printf("LONG_MAX\t%ld\n", LONG_MAX);
 	printf("LONG_MIN\t%ld\n", LONG_MIN);
 	printf("SCHAR_MAX\t%d\n", SCHAR_MAX);
@@ -26,74 +27,95 @@ int main(void)
 	printf("ULLONG_MAX\t%llu\n", ULLONG_MAX);
 	printf("USHRT_MAX\t%u\n", USHRT_MAX);
 	
+	//print values from computation
 	printf("\nfrom computation:\n");
+	
 	// CHAR_BIT
 	int charbit;
 	unsigned char maxuchar, testuchar;
+	maxuchar = 0;
 	for (testuchar=1; testuchar > maxuchar; ++testuchar)  // test overflows; max is the max value
 		maxuchar = testuchar;
-	testuchar = maxuchar;  // begin bit caluclation
-	for (charbit=1; (testuchar/2) > 1; ++charbit)
+	testuchar = maxuchar;  // begin bit caluclation on variable testuchar
+	for (charbit=1; testuchar > 1; ++charbit)
 		testuchar = testuchar / 2;
 	printf("CHAR_BIT\t%d\n", charbit);
+	
 	// CHAR_MAX
 	char testchar, maxchar;
+	maxchar = 0;
 	for (testchar=1; testchar > maxchar; ++testchar)  // test overflows; max is the max value
 		maxchar = testchar;
 	printf("CHAR_MAX\t%d\n", (int) maxchar);
 	// CHAR_MIN
 	printf("CHAR_MIN\t%d\n", (int) testchar);
+	
 	// INT_MAX
 	int maxint, testint;
+	maxint = 0;
 	for (testint=1; testint > maxint; ++testint)
 		maxint = testint;
-	printf("INT_MAX\t%d\n", maxint);
+	printf("INT_MAX \t%d\n", maxint);
 	// INT_MIN
-	printf("INT_MIN\t%d\n", testint);
+	printf("INT_MIN \t%d\n", testint);
+	
 	// LONG_MAX
 	long maxlong, testlong;
+	maxlong = 0;
 	for (testlong=1; testlong > maxlong; ++testlong)
 		maxlong = testlong;
 	printf("LONG_MAX\t%ld\n", maxlong);
 	// LONG_MIN
 	printf("LONG_MIN\t%ld\n", testlong);
+	
 	// SCHAR_MAX
 	char maxschar, testschar;
+	maxschar = 0;
 	for (testschar=1; testschar > maxschar; ++testschar)
 		maxschar = testschar;
 	printf("SCHAR_MAX\t%d\n", (int) maxschar);
 	// SCHAR_MIN
 	printf("SCHAR_MIN\t%d\n", (int) testschar);
+	
 	// SHRT_MAX
 	short maxshrt, testshrt;
+	maxshrt = 0;
 	for (testshrt=1; testshrt > maxshrt; ++testshrt)
 		maxshrt = testshrt;
 	printf("SHRT_MAX\t%d\n", maxshrt);
 	// SHRT_MIN
 	printf("SHRT_MIN\t%d\n", testshrt);
+	
 	// UCHAR_MAX
 	printf("UCHAR_MAX\t%d\n", (int) maxuchar);
-	// UINT_MAX
-	unsigned int maxuint, testuint;
-	for (testuint=1; testuint > maxuint; ++testuint)
-		maxuint = testuint;
-	printf("UINT_MAX\t%u\n", maxuint);
-	// ULONG_MAX
-	unsigned long maxulong, testulong;
-	for (testulong=1; testulong > maxulong; ++testulong)
-		maxulong = testulong;
-	printf("ULONG_MAX\t%lu\n", maxulong);
-	// ULLONG_MAX
-	unsigned long long maxullong, testullong;
-	for (testullong=1; testullong > maxullong; ++testullong)
-		maxullong = testullong;
-	printf("ULLONG_MAX\t%llu\n", maxullong);
+	
 	// USHRT_MAX
 	unsigned short maxushrt, testushrt;
+	maxushrt = 0;
 	for (testushrt=1; testushrt > maxushrt; ++testushrt)
 		maxushrt = testushrt;
 	printf("USHRT_MAX\t%u\n", (int) maxushrt);
+	
+	// UINT_MAX
+	unsigned int maxuint, testuint;
+	maxuint = 0;
+	for (testuint=1; testuint > maxuint; ++testuint)
+		maxuint = testuint;
+	printf("UINT_MAX\t%u\n", maxuint);
+	
+	// ULONG_MAX
+	unsigned long maxulong, testulong;
+	maxulong = 0;
+	for (testulong=1; testulong > maxulong; ++testulong)
+		maxulong = testulong;
+	printf("ULONG_MAX\t%lu\n", maxulong);
+	
+	// ULLONG_MAX
+	unsigned long long maxullong, testullong;
+	maxullong = 0;
+	for (testullong=1; testullong > maxullong; ++testullong)
+		maxullong = testullong;
+	printf("ULLONG_MAX\t%llu\n", maxullong);
 
 	return 0;
-	
 }
