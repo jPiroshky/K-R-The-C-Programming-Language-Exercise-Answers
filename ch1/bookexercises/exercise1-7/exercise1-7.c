2be2a2834c0f55c9d1312f3af0b59f59850ff63c
John Pierce Parker <jPiroshky@gmail.com>
2012-03-03 20:07:01 -0800

    completed exercise1-7

---
+++ ch1/bookexercises/exercise1-7/exercise1-7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* exercise1-7 - Write a program to print the value of EOF.
+ * 
+ * exercise1-6.c - A program that prints the integer value of the EOF
+ * character. */
+main()
+{
+		printf("Integer value of EOF character: %d\n", EOF);
+}
