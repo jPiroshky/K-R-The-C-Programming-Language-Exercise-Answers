a4098a6861392c5559acd04a42264731d16c17f7
John Pierce Parker <jPiroshky@gmail.com>
2012-03-19 12:17:11 -0700

    added personalexercise stringlengthtest.c to ch1

---
+++ ch1/personalexercises/stringlengthtest.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* stringlengthtest.c - take a string stored in a character array, then
+ * replace a character in the middle with a null character, and print
+ * the result
+ * 
+ * Purpose:
+ * to see the result of printing a string with characters after a null
+ * character*/
+ 
+ int main()
+ {
+	//define and print the initial string
+	char line[] = "this is the string.\n";
+	printf("%s", line);
+	
+	//edit the string
+	printf("editing string...\n");
+	line[8] = '\0';
+	
+	//print the edited string
+	printf("%s", line);
+	 
+	return 0;
+}
