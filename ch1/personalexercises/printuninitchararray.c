8662b0533705679849247ed914f4fdda5d9a3a3e
John Pierce Parker <jPiroshky@gmail.com>
2012-03-21 11:32:06 -0700

    fixed comments in printuninitchararray.c

--- ch1/personalexercises/printuninitchararray.c
+++ ch1/personalexercises/printuninitchararray.c
@@ -5,6 +5,12 @@
 /* printinitchararray.c - declares a character array of 'LINELENGTH'
  * elements, and then prints the content of the array before the array
  * is initialized
+ * 
+ * Purpose: To see the contents of an unitialized character array.
+ * 
+ * Conclusion: The array prints what is stored in the memory before the
+ * program is run, and so can be different every time the program is
+ * run. */
 int main()
 {
 	char line[LINELENGTH];		//declare a char array of 'LINELENGTH' elements
