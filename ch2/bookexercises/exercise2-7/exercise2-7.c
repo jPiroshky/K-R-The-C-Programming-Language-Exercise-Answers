fa57e55fc7ae41fc9499e3a1f46fd5bc76a41bd2
John Pierce Parker <jPiroshky@gmail.com>
2012-04-08 21:44:08 -0700

    completed exercise2-7

--- ch2/bookexercises/exercise2-7/exercise2-7.c
+++ ch2/bookexercises/exercise2-7/exercise2-7.c
@@ -5,24 +5,22 @@
 
 unsigned setbits(unsigned x, int p, int n, unsigned y);
 
-/* exercise2-6 - Write a function setbits(x,p,n,y) that returns x with
- * the n bits that begin at position p set to the rightmost n bits of y,
- * leaving the other bits unchanged. 
+/* exercise2-7 - Write a function invert(x,p,n) that returns x with the
+ * n bits that begin at position p set to the rightmost n bits of y,
+ * leaving the other bits unchanged.
  * 
- * exercise2-6.c - prompts the user for values, applies 
- * setbits(x,p,n,y), and the prints the result.  Will not return an
- * error if given non-decimal-number input. 
- * 
- * NOTE: This implementation begs a more elegant solution. */
+ * exercise2-7.c - prompts the user for values, applies invert(x,p,n),
+ * and the prints the result.  Will not return an error if given
+ * non-decimal-number input. */
 int main()
 {
         char numstring[MAXLINE];
         
-        unsigned x, y;
+        unsigned x;
         int p, n;
         
         //prompt user for x (number to transform)
-        printf("enter a number to apply setbits(x, p, n, y) to: ");
+        printf("enter a number to apply invert(x, p, n) to: ");
         gets(numstring);
         x = (unsigned) atoi(numstring);
         
@@ -35,31 +33,20 @@ int main()
         printf("enter a number n corresponding to the number of digits: ");
         gets(numstring);
         n = atoi(numstring);
-        
-        //prompt user for y (transformant)
-        printf("enter a number y to apply the righmost n bits from: ");
-        gets(numstring);
-        y = (unsigned) atoi(numstring);
-        
+                
         //print result
-        printf("\nThe resulting transformation of x is: %u\n", setbits(x, p, n, y));
+        printf("\nThe resulting transformation of x is: %u\n", invert(x, p, n));
         
         return 0;
 }
 
-/* setbits() - set the n bits of variable x starting at position p to 
- * the first n bits (rightmost n bits) of variable y. */
-unsigned setbits(unsigned x, int p, int n, unsigned y)
-{       
-        /* set active bits in x to 0, then apply mask with active bits in y
-         * shifted into position */
-        // (~(~(~0 << n) << (p+1-n))) creates a mask with 0s in the active bits of x
-        // ((y & ~(~0 << n)) << (p+1-n)) rightmost n bits of y shifted into place
-        return x & (~(~(~0 << n)<<(p+1-n))) | ((y & ~(~0 << n)) << (p+1-n));
-}
-
+/* invert() - inverts n bits in x starting at position p (counting back
+ * to origin) */
 unsigned invert(unsigned x, int p, int n)
 {
-    /* turn off active bits in x, and then turn on inverted active bits in x */
-    return (x & (~(~(~0 << n)<<(p+1-n)))) | (~x & (~(~0 << n) << (p+1-n)));
+    /* turn off active bits in x, and then turn on inverted active bits
+     * in x */
+    // (x & (~(~(~0 << n)<<(p+1-n)))) turns off active bits in x
+    // (~x & (~(~0 << n)<<(p+1-n))) creates layer with inverted bits in place
+    return (x & (~(~(~0 << n)<<(p+1-n)))) | (~x & (~(~0 << n)<<(p+1-n)));
 }
