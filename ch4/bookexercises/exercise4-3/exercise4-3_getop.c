cf4c55aefd23294dd28da50f61c8dca6256cac24
John Pierce Parker <jpiroshky@gmail.com>
2012-06-02 09:11:15 -0700

    added exercise4-3; need update ch4contents.txt

---
+++ ch4/bookexercises/exercise4-3/exercise4-3_getop.c
@@ -0,0 +1,31 @@
+#include <ctype.h>
+#include "exercise4-3_globals.h"
+
+#define EOF -1
+
+int getch(void);
+void ungetch();
+
+/* getop: get next character on numeric operand */
+int getop(char s[])
+{
+	int i, c;
+	
+	while ((s[0] = c = getch()) == ' ' || c == '\t')
+		;
+	s[1] = '\0';
+	if (!isdigit(c) && c != '.')
+		return c;            /* not a number */
+	i = 0;
+	if (isdigit(c))          /* collect integer part */
+		while (isdigit(s[++i] = c = getch()))
+			;
+	if (c == '.')
+		while (isdigit(s[++i] = c = getch()))
+			;
+	s[i] = '\0';
+	if (c != EOF)
+		ungetch(c);
+	
+	return NUMBER;
+}
---
+++ ch4/bookexercises/exercise4-3/exercise4-3_globals.h
@@ -0,0 +1,1 @@
+#define NUMBER   '0'  /* signal that number was found */
